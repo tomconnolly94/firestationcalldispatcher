f73b1a6790bcc9b34ff9501cd197940c481612b6
tom <tom.connolly@protonmail.com>
2021-01-26 21:55:36 +0100

    ADDED: CallFactory static class and test

---
+++ FireStationCallDispatcher/CallFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FireStationCallDispatcher
+{
+    public static class CallGenerator
+    {
+        public static List<Call> GenerateCalls(int callCount)
+        {
+            List<Call> calls = new List<Call>();
+            Random random = new Random();
+
+            for (int callIndex = 0; callIndex < callCount; callIndex++)
+            {
+                var priorityIndex = random.Next(0, Enum.GetNames(typeof(PriorityLevel)).Length);
+                PriorityLevel callPriority = (PriorityLevel)priorityIndex;
+                calls.Add(new Call(callPriority, callIndex + 1, $"Caller No {callIndex + 1}"));
+            }
+
+            Logger.InfoLog($"Calls created. {callCount} calls in the call queue.");
+            return calls;
+        }
+    }
+}
---
+++ FireStationCallDispatcherTest/CallFactoryTest.cs
@@ -0,0 +1,26 @@
+﻿using FireStationCallDispatcher;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FireStationCallDispatcherTest
+{
+    [TestClass]
+    public class CallGeneratorTest
+    {
+        [TestMethod]
+        public void TestGenerateCalls()
+        {
+            int callCount = 10;
+
+            List<Call> calls = CallGenerator.GenerateCalls(callCount);
+
+            Assert.AreEqual(callCount, calls.Count);
+            foreach(Call call in calls)
+            {
+                Assert.IsNotNull(call.CallerName);
+                Assert.IsNotNull(call.CallId);
+                Assert.IsNotNull(call.CallPriority);
+            }
+        }
+    }
+}
